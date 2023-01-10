2752c8214a139abada003da9a346d0badd873190
Geert Verbist <bist@bist.be>
2023-01-10 08:38:51 +0100

    Added function to add the targets dictionary to the sqlite database. -Basic logic- needs work

--- main.py
+++ main.py
@@ -37,6 +37,8 @@ engine = sqlalchemy.create_engine(
 metadata.create_all(engine)
 #end of the database creating block.
 
+#Function that matches the players firstname with a target list that has made a copy of the firstname list.
+
 def assign_players(players):
   # Make a copy of the list of players, so we can remove players from it as we assign them
   players_copy = players.copy()
@@ -63,7 +65,56 @@ def assign_players(players):
 
 # Test the function
 print(assign_players(['Fanny', 'Bist', 'Jornt', 'Vince','Nancy', 'Noekie']))  # Should print {'Alice': 'Charlie', 'Bob': 'Alice', 'Charlie': 'Bob'}
-print(assign_players(['Fanny', 'Bist', 'Jornt', 'Vince','Nancy', 'Noekie', 'Alice', 'Bob', 'Charlie', 'Dave']))  # Should print a dictionary with the players randomly assigned to each other
+assigned_dict=assign_players(['Fanny', 'Bist', 'Jornt', 'Vince','Nancy', 'Noekie', 'Alice', 'Bob', 'Charlie', 'Dave'])  # Should print a dictionary with the players randomly assigned to each other
+
+#end of assignment function
+
+
+
+
+
+#function that takes a dictionary and adds the values from the dictionary to the target column of 
+#the players table in the database. Need some extra logic because this function can only run once.
+#should use the dictionary from the assign_players function as input for this function
+
+def add_assignments_to_db(assignments):
+    Base = declarative_base()
+    class Players(Base):
+        __tablename__ = 'players'
+        id = Column(Integer, primary_key=True)
+        firstname = Column(String)
+        target = Column(String)
+
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for player, target in assignments.items():
+        session.add(Players(target=target))
+    
+    session.commit()
+
+# add_assignments_to_db(assigned_dict)
+
+ 
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
 
 
 
